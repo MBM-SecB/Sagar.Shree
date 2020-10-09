bf7b1499eebf72daa206d60475738d5a98b6c608
shree <shreesagar48@gmail.com>
2020-10-09 11:54:11 +0545

    oct 9 final

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -5,26 +5,54 @@ public class EmployeeController : Controller
 {
     public ActionResult Index()
     {
-        List<Employee> employees = new List<Employee>();
         var employee1 = new Employee
         {
             FirstName = "Sagar",
             Surname = "Shree",
             Address = "Dang The Capital City",
-            Salary = 1,
+            Salary = 100000,
         };
-        return View();
+        var employee2 = new Employee
+        {
+            FirstName = "Madan",
+            Surname = "Devkota",
+            Address = "Salyan",
+            Salary = 10000,
+        };
+        var employee3 = new Employee
+        {
+            FirstName = "Sita",
+            Surname = "Bista",
+            Address = "Butwal the nabhako capital",
+            Salary = 15000,
+        };
+        var employee4 = new Employee
+        {
+            FirstName = "Hari ",
+            Surname = "Nepal",
+            Address = "Kathmandu",
+            Salary = 20000,
+        };
+        var employee5 = new Employee
+        {
+            FirstName = "Josh",
+            Surname = "Shrestha",
+            Address = "Bhaktapur",
+            Salary = 16000,
+        };
+
+        var employees = new List<Employee> { employee1, employee2, employee3, employee4, employee5 };
+
+
+        // employees.Add(employee1);
+        // employees.Add(employee2);
+        // employees.Add(employee3);
+        // employees.Add(employee4);
+        // employees.Add(employee5);
+
+
+
+        return View(employees);
     }
 }
 
-public class Employee
-{
-    public string FirstName { get; set; }
-    public string Surname { get; set; }
-
-    public string Address { get; set; }
-    public char Gender { get; set; } = 'M';
-    public double Salary { get; set; }
-
-
-}
