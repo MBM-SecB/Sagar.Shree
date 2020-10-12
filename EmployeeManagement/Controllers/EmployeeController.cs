356249f25415d1edebe21ca99c2e0ab198df3bf7
shree <shreesagar48@gmail.com>
2020-10-12 11:53:14 +0545

    Added employee details

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,58 +1,25 @@
-using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 public class EmployeeController : Controller
 {
     public ActionResult Index()
     {
-        var employee1 = new Employee
-        {
-            FirstName = "Sagar",
-            Surname = "Shree",
-            Address = "Dang The Capital City",
-            Salary = 100000,
-        };
-        var employee2 = new Employee
-        {
-            FirstName = "Madan",
-            Surname = "Devkota",
-            Address = "Salyan",
-            Salary = 10000,
-        };
-        var employee3 = new Employee
-        {
-            FirstName = "Sita",
-            Surname = "Bista",
-            Address = "Butwal the nabhako capital",
-            Salary = 15000,
-        };
-        var employee4 = new Employee
-        {
-            FirstName = "Hari ",
-            Surname = "Nepal",
-            Address = "Kathmandu",
-            Salary = 20000,
-        };
-        var employee5 = new Employee
-        {
-            FirstName = "Josh",
-            Surname = "Shrestha",
-            Address = "Bhaktapur",
-            Salary = 16000,
-        };
-
-        var employees = new List<Employee> { employee1, employee2, employee3, employee4, employee5 };
-
-
-        // employees.Add(employee1);
-        // employees.Add(employee2);
-        // employees.Add(employee3);
-        // employees.Add(employee4);
-        // employees.Add(employee5);
-
+        var employees = Employee.GetEmployees();
 
 
         return View(employees);
+    }
+
+    public ActionResult Details(string firstName)
+    {
+        var employees = Employee.GetEmployees();
+        // var employee = employees.Where(x => x.FirstName == firstName).First();
+        // var employee = employees.First(x => x.FirstName == firstName);
+        var employee = employees.FirstOrDefault(x => x.FirstName == firstName);
+
+
+        return View(employee);
     }
 }
 
--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 public class Employee
 {
     public string FirstName { get; set; }
@@ -7,5 +9,46 @@ public class Employee
     public char Gender { get; set; } = 'M';
     public double Salary { get; set; }
 
+    public static List<Employee> GetEmployees()
+    {
+        var employee1 = new Employee
+        {
+            FirstName = "Sagar",
+            Surname = "Shree",
+            Address = "Dang The Capital City",
+            Salary = 100000,
+        };
+        var employee2 = new Employee
+        {
+            FirstName = "Madan",
+            Surname = "Devkota",
+            Address = "Salyan",
+            Salary = 10000,
+        };
+        var employee3 = new Employee
+        {
+            FirstName = "Sita",
+            Surname = "Bista",
+            Address = "Butwal the nabhako capital",
+            Salary = 15000,
+        };
+        var employee4 = new Employee
+        {
+            FirstName = "Hari ",
+            Surname = "Nepal",
+            Address = "Kathmandu",
+            Salary = 20000,
+        };
+        var employee5 = new Employee
+        {
+            FirstName = "Joshi",
+            Surname = "Shrestha",
+            Address = "Bhaktapur",
+            Salary = 16000,
+        };
+
+        var employees = new List<Employee> { employee1, employee2, employee3, employee4, employee5 };
+        return employees;
+    }
 
 }
