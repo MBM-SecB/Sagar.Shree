55da89ef5ac7c719e3aa26dc786fb291a3d399e1
shree <shreesagar48@gmail.com>
2020-12-02 10:43:34 +0545

    added delete employee

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -4,19 +4,22 @@ using Microsoft.AspNetCore.Mvc;
 
 public class EmployeeController : Controller
 {
+    private EMSContext db;
+
+    public EmployeeController(EMSContext _db)
+    {
+        db = _db;
+    }
+
     public ActionResult Index()
     {
-        var db = new EMSContext();
-        var employees = db.Employees.Select(x => x.Gender == 'm');
-
+        var employees = db.Employees.ToList();
         return View(employees);
     }
 
-    public ActionResult Detail(string firstName)
+    public ActionResult Detail(int id)
     {
-        var employees = Employee.GetEmployees();
-        var employee = employees.FirstOrDefault(x => x.FirstName == firstName);
-
+        var employee = db.Employees.Find(id);
         return View(employee);
     }
 
@@ -26,8 +29,21 @@ public class EmployeeController : Controller
     }
 
     [HttpPost]
-    public ActionResult<bool> Add(Employee employee)  // Model binding
+    public ActionResult Add(Employee employee)  // Model binding
     {
-        return true;
+        db.Employees.Add(employee);
+        db.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    public ActionResult Delete(int id)
+    {
+        var employee = db.Employees.Find(id);
+        db.Employees.Remove(employee);
+        db.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
     }
 }
---
+++ LearnSqlite/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace LearnSqlite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionStringBuilder = new SqliteConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "./myDb.db";
+
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CREATE;
+                tableCmd.CommandText = "CREATE TABLE favourire_beers(name VARCHAR(50)) ";
+
+            };
+
+
+
+        }
+    }
+}
